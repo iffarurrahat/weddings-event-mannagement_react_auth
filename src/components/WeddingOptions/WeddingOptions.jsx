1d0783b55c45f2ad2f297d353f659f15acc9632a
Iffarur <iffarurrahat@gmail.com>
2023-10-10 17:50:17 +0600

    added aos effect

--- src/components/WeddingOptions/WeddingOptions.jsx
+++ src/components/WeddingOptions/WeddingOptions.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
 import WeddingOptionsCard from './WeddingOptionsCard';
 
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 const WeddingOptions = ({ services }) => {
+
+    useEffect(() => {
+        Aos.init();
+    }, [])
 
     return (
         <div className='mt-10'>
-            <div className='text-center mb-12'>
+            <div className='text-center mb-12' data-aos="fade-top">
                 <h2 className='text-3xl font-semibold mb-3'>Wedding Styles</h2>
                 <p>A wedding coordinator is a professional who assists couples in planning <br /> and executing their wedding day, ensuring a smooth and memorable event.</p>
             </div>
-            <div className='max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 px-2 md:px-3 lg:px-1'>
+            <div className='max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 px-2 md:px-3 lg:px-1' data-aos="fade-bottom">
                 {
                     services?.map(service => <WeddingOptionsCard key={service.id} service={service}></WeddingOptionsCard>)
                 }
--- src/components/WeddingOptions/WeddingOptionsCard.jsx
+++ src/components/WeddingOptions/WeddingOptionsCard.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+
 const WeddingOptionsCard = ({ service }) => {
     const { id, image, title, description, price } = service || {}
 
